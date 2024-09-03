543037721f72d276f9ac9800638169e01abd9ab1
JeanRodriguez18 <jeancarlosrt301@gmail.com>
2024-09-03 17:02:01 -0400

    Upgrade web api to .net core 3.1 web api

---
+++ RMApi/Controllers/InventoryController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RMDataManager.Library.DataAccess;
+using RMDataManager.Library.Models;
+using System.Collections.Generic;
+
+namespace RMApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class InventoryController : ControllerBase
+    {
+        [Authorize(Roles = "Manager,Admin")]
+        public List<InventoryModel> GetInventory()
+        {
+            InventoryData data = new InventoryData();
+
+            return data.GetInventory();
+        }
+
+        [Authorize(Roles = "Manager,Admin")]
+        public void Post(InventoryModel model)
+        {
+            InventoryData data = new InventoryData();
+
+            data.SaveInventoryRecord(model);
+        }
+    }
+}
